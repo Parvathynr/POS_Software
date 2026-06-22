a794ec25a412ca0775859f799739e48f4cd1a6f5
Parvati <parvathynair186@gmail.com>
2026-06-22 17:39:46 +0530

    'cors-updated'

--- POS/settings.py
+++ POS/settings.py
@@ -62,13 +62,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'POS.urls'
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://pos-software-axalin.netlify.app"
 ]
 
 TEMPLATES = [
